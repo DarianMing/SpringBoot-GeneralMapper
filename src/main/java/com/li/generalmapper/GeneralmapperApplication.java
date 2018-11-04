56056ce7d8080fddf6579836a28f589a65af0608
DarainMing <743310059@qq.com>
2018-11-04 17:50:56 +0800

    init project

---
+++ src/main/java/com/li/generalmapper/GeneralmapperApplication.java
@@ -0,0 +1,12 @@
+package com.li.generalmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralmapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralmapperApplication.class, args);
+    }
+}
